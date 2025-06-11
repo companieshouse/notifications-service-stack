5337fe9cbb08e57f14d331d07125bf2470cfca2a
rhilgenbergbastos-ch <rhilgenbergbastos@companieshouse.gov.uk>
2025-06-11 15:52:34 +0100

    style: format

--- groups/infrastructure/locals.tf
+++ groups/infrastructure/locals.tf
@@ -3,13 +3,13 @@ locals {
   stack_fullname = "${local.stack_name}-stack"
   name_prefix    = "${local.stack_name}-${var.environment}"
 
-  stack_secrets              = jsondecode(data.vault_generic_secret.secrets.data_json)
+  stack_secrets = jsondecode(data.vault_generic_secret.secrets.data_json)
 
-  application_subnet_pattern  = local.stack_secrets["application_subnet_pattern"]
-  application_subnet_ids      = join(",", data.aws_subnets.application.ids)
-  kms_key_alias               = local.stack_secrets["kms_key_alias"]
-  vpc_name                    = local.stack_secrets["vpc_name"]
-  notify_topic_slack_endpoint = local.stack_secrets["notify_topic_slack_endpoint"]
+  application_subnet_pattern      = local.stack_secrets["application_subnet_pattern"]
+  application_subnet_ids          = join(",", data.aws_subnets.application.ids)
+  kms_key_alias                   = local.stack_secrets["kms_key_alias"]
+  vpc_name                        = local.stack_secrets["vpc_name"]
+  notify_topic_slack_endpoint     = local.stack_secrets["notify_topic_slack_endpoint"]
   ingress_cidrs_private           = concat(local.management_private_subnet_cidrs, local.application_cidrs)
   ingress_prefix_list_ids         = [data.aws_ec2_managed_prefix_list.admin_cidr.id]
   subnet_ids_private              = join(",", data.aws_subnets.private.ids)
@@ -22,7 +22,7 @@ locals {
   )
 
 
-  parameter_store_secrets    = {
+  parameter_store_secrets = {
     "web-oauth2-client-id"     = local.stack_secrets["web-oauth2-client-id"],
     "web-oauth2-client-secret" = local.stack_secrets["web-oauth2-client-secret"],
     "web-oauth2-cookie-secret" = local.stack_secrets["web-oauth2-cookie-secret"],
